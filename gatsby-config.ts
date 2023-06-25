510ca51d9e81a290ebaa5cc4510f2a217398fcef
Karen Ziboh <karen@cchubnigeria.com>
2023-06-25 15:36:49 +0100

    add config files

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,18 @@
-import type { GatsbyConfig } from "gatsby";
+import type {GatsbyConfig} from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: 'Portfolio',
+    siteUrl: 'https://www.yourdomain.tld',
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-sitemap"]
+  plugins: [
+    `gatsby-plugin-sharp`,
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-image',
+    'gatsby-transformer-sharp',
+  ],
 };
 
 export default config;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
     `./src/pages/**/*.{js,jsx,ts,tsx}`,
     `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        calibre: ['Calibre'],
+        mono: ['SFMono'],
+      },
+      colors: {
+        lemon: '#64FFDA',
+        gray: '#8892B0',
+        'blue-50': '#CCD6F6',
+        'blue-100': '#0A192F',
+        'blue-200': '#112240',
+      },
+      screens: {
+        sm: '540px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1440px',
+        '2xl': '1728px',
+        'max-xl': '1926px',
+      },
+    },
   },
   plugins: [],
-}
+};
